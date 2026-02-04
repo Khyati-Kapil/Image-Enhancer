048f4f0bd57e24aae5bc63b05214a1da95f98494
Khyati-Kapil <khyatekapil@gmail.com>
2026-02-04 19:38:45 +0530

    feat: implement image enhancement feature with API integration

--- image-enhancer/src/components/Home.jsx
+++ image-enhancer/src/components/Home.jsx
@@ -1,32 +1,29 @@
-import ImageUpload from "./ImageUpload"
-import ImagePreview from "./ImagePreview"
+import ImageUpload from "./ImageUpload";
+import ImagePreview from "./ImagePreview";
 import { useState } from "react";
+import { enhancedImageAPI } from "../utils/enhanceImageApi";
 
+const Home = () => {
+    const [uploadImage, setUploadImage] = useState(null);
+    const [enhancedImage, setEnhancedImage] = useState(null);
+    const [loading, setloading] = useState(false);
 
-
-const Home=()=>{
-    const[uploadImage,setUploadImage] = useState(null)
-    const[enhancedImage,setEnhancedImage] = useState(null)
-    const[loading , setLoading] = useState(false)
-
-    const UploadImageHandler = async (file)=>{
-        setUploadImage(URL.createObjectURL(file))
-        setLoading(true)
-        try{
+    const UploadImageHandler = async (file) => {
+        setUploadImage(URL.createObjectURL(file));
+        setloading(true);
+        try {
             const enhancedURL = await enhancedImageAPI(file);
-            setEnhancedImage(enhancedURL)
-            setLoading(false)
-
-        }catch(error){
-            console.log(error)
-            alert("Please Try again")
-
+            setEnhancedImage(enhancedURL);
+            setloading(false);
+        } catch (error) {
+            console.log(error);
+            alert("Error while enhancing the image. Please try again later.");
         }
-        
-    }
+    };
+
     return (
         <>
-            <ImageUpload UploadImageHandler={UploadImageHandler}/>
+            <ImageUpload UploadImageHandler={UploadImageHandler} />
             <ImagePreview
                 loading={loading}
                 uploaded={uploadImage}
